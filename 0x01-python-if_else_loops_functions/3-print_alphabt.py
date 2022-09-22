3d15415bcb1e4c9fe8fc50e88a162eb9d39994b2
paulsunday <paulsundayigwegbe@gmail.com>
2022-09-21 21:29:57 -0700

    changes

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-for ch in range(97, 123):
-    if chr(ch) == 'e' or chr(ch) == 'q':
-        continue
-    else:
-        print("{0}".format(chr(ch)), end="")
--- 0x01-python-if_else_loops_functions/7-islower.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def islower(c):
-    '''checks for lower case characters and returns true if lower'''
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True
-    else:
-        return False
