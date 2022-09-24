65b9d6da10938e9b740005855fed159ecc4a3267
paulsunday <paulsundayigwegbe@gmail.com>
2022-09-24 06:21:31 -0700

    FIX

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
@@ -12,4 +13,3 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-
