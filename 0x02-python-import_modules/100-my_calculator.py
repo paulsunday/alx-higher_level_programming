21576405fee0b6acd1d33f29202523219e63cc2b
paulsunday <paulsundayigwegbe@gmail.com>
2022-09-24 04:58:47 -0700

    FIX

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) != 4:
-        print("Usage: ./100-mycalculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         quit(1)
         a = int(argv[1])
         b = int(argv[3])
-        ops = ["+", "_", "*", "/"]
+        ops = ["+", "-", "*", "/"]
         from calculator_1 import add, sub, mul, div
         funcs = [add, sub, mul, div]
         for i, s in enumerate(ops):
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,10 @@
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
+
     count = len(sys.argv) - 1
     if count == 0:
-        print("0 arguments:")
+        print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
