3b16218a0e351cc39c3f56d578337edcdd7456b3
paulsunday <paulsundayigwegbe@gmail.com>
2022-09-26 20:52:02 -0700

    SOLUTION

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return None
     for item in my_list[::-1]:
         print("{:d}".format(item))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copied_list = my_list.copy()
-    if idx < 0 or idx > len(my_list) -1:
+    if idx < 0 or idx > len(my_list) - 1:
         return copied_list
     else:
         copied_list[idx] = element
