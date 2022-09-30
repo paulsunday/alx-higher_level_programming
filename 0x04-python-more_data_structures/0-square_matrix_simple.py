974968103b96ff48571d57deb38b6e85e936f47b
paulsunday <paulsundayigwegbe@gmail.com>
2022-09-29 20:49:46 -0700

    TASKS

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
-#!/usr/python3
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map((lambda x: x * x), els)) for elm in matrix]
+    return [list(map((lambda x: x * x), elm)) for elm in matrix]
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    for i in range(len(my_list) - 1):
-        if new_list[1] == search:
-            new_list[i] = replace
-    return new_list
+    def s_r_elm(elm):
+        return (elm if elm != search else replace)
+    return list(map(s_r_elm, my_list))
