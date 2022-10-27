f688142ad733f5a373841c49e4c0c7770ff3f6cc
paulsunday <paulsundayigwegbe@gmail.com>
2022-10-27 01:31:24 -0700

    Updated Tasks

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module: 1-my_list """
+class MyList(list):
+    """ Represents a MyList """
+
+    def print_sorted(self):
+        """ Prints the list, but sorted """
+        print(sorted(self))
