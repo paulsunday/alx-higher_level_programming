f2d7c2c604d41f3de90506c7a7a28968e72a1e3b
paulsunday <paulsundayigwegbe@gmail.com>
2022-12-22 20:38:47 -0800

    Task 103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""Defines a class MagicClass"""
+import math
 
 
-class Square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
-    """
-    def __init__(self, size=0):
-        """initializes the square
-        Args:
-            size (int): size of a side of the square
-        Returns:
-            None
-        """
-        self.size = size
+class MagicClass:
+    """This reperensents a circle"""
+    def __int__(self, radius=0):
+        """Initializes the magix class"""
+        self.radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
 
     def area(self):
         """calculates the square's area
