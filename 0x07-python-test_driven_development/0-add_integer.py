a5cb6683046c1f237c48072e94078351db0446e7
paulsunday <paulsundayigwegbe@gmail.com>
2022-12-23 23:10:36 -0800

    TAsk 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-Function that does addition and subtraction
+This is the "0-add_integer" module.
+THe 0-add_integer module supplies one function, add_integer(a, b).
 """
 def add_integer(a, b=98):
-    """ A and B must be int or float
-    else raise error msg
-    """
+    """ Return the addition of two numbers."""
     if type(a) is not int and type(a) is not float:
         raise TypeError ("a must be an integer")
     elif type(b) is not int and type(b) is not float:
         raise TypeError ("b must be an integer")
     elif type(a) is float:
-        int = int(a)
+        a = int(a)
     elif type(b) is float:
-        int = int(b)
+        b = int(b)
     return a + b
 
