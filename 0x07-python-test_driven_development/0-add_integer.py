c22d385a86bbe39b7e7c5dac4be18814fceb9071
paulsunday <paulsundayigwegbe@gmail.com>
2022-12-23 23:20:05 -0800

    Task 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,11 +7,11 @@ def add_integer(a, b=98):
     """ Return the addition of two numbers."""
     if type(a) is not int and type(a) is not float:
         raise TypeError ("a must be an integer")
-    elif type(b) is not int and type(b) is not float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError ("b must be an integer")
-    elif type(a) is float:
+    if type(a) is float:
         a = int(a)
-    elif type(b) is float:
+    if type(b) is float:
         b = int(b)
     return a + b
 
