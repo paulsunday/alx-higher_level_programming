dc65fa9d849d1af7e7f81ebf4dff2aa360f3b287
paulsunday <paulsundayigwegbe@gmail.com>
2023-01-11 00:53:09 -0800

    TASKS

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is a class that is inherited from another class
+"""
+
+
+class BaseGeometry:
+    """
+    This class raises an exception error because thre area was not defined
+    """
+    raise Exception("area() is not implemented")
