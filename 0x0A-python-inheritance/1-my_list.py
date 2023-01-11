fd4733bab8f022915379924978b650079fd91c8e
paulsunday <paulsundayigwegbe@gmail.com>
2023-01-10 19:29:26 -0800

    FIX

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,9 +2,11 @@
 """ 
 Module: 1-my_list
 """
+
 class MyList(list):
     """ Represents a MyList """
 
     def print_sorted(self):
         """ Prints the list, but sorted """
+
         print(sorted(self))
