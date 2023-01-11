bebd269fbf84c5faf52d5fdcfffeba6e021f82cd
paulsunday <paulsundayigwegbe@gmail.com>
2023-01-11 01:21:58 -0800

    TASK

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,5 +6,5 @@ This is a class that defines an area inherited from another class
 
 class BaseGeometry:
     """Raise an Exception on the area attribute"""
-    def area():
+    def area(self):
         raise Exception("area() is not implemented")
