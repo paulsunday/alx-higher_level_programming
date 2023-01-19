a969113d0f0bca93cf651ab6bdfb20a497a06cd9
paulsunday <paulsundayigwegbe@gmail.com>
2023-01-18 20:44:14 -0800

    FIX

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-	"""defines a txt file using the UTF-8 encoding format"""
-	with open(filename, encoding="utf-8") as f:
-	print(f.read())	
+    """defines a txt file using the UTF-8 encoding format"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+THis is the tasks on file manipulation
