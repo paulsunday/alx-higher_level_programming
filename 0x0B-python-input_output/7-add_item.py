78e62d4f4bee99d773e31ce55a921a46a21fdf8f
paulsunday <paulsundayigwegbe@gmail.com>
2023-01-24 13:37:56 -0800

    TASKS

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A script that add all arg in a pythin list
+and save them in a file"""
+
+from sys import argv
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    json_list = load_from_json_file(filename)
+except FileNotFoundError:
+    json_list = []
+
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, filename)
