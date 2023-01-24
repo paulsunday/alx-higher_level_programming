c71ed2f9baff96b5ef2fcd58f0351b8f2212de53
paulsunday <paulsundayigwegbe@gmail.com>
2023-01-23 23:29:13 -0800

    TASKS

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
 contains a function that writes into a file
 """
 
+
 def write_file(filename="", text=""):
     """Write a file with the keyword"""
     with open(filename, "w", encoding="UTF-8") as f:
