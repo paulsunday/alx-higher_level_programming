eabb204034f662dbb211ccd1555c01dbdaf435dc
paulsunday <paulsundayigwegbe@gmail.com>
2023-01-23 16:50:46 -0800

    TASKS

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This function writes string to a text file using UTF8"""
+
+
+def write_file(filename="", text=""):
+    """Write a file with the keyword"""
+    with open(filename, "w", encoding="UTF-8")
+    return f.write(text)
