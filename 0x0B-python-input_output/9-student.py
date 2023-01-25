5c27065b78be1efee6117b880c3181e8ce530331
paulsunday <paulsundayigwegbe@gmail.com>
2023-01-24 20:22:18 -0800

    TASKS

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class that defines a student"""
+
+
+class Student:
+    """Has a public instance that has 
+    first name, last name and age of the student"""
+    
+    def __init__(self, first_name, last_name, age):
+        """The public inistialization of the student class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """A public method that retrives a dict repr of a student"""
+        return self.__dict__
