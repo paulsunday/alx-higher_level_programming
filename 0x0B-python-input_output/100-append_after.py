1c1daf78a918dda8fd1e61d184622ad423c080ab
paulsunday <paulsundayigwegbe@gmail.com>
2023-01-29 00:12:38 -0800

    TASKS

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,8 +3,8 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """appends "new_string" after a line 
-    containing "search_string" in "fileName" """
+    """appends "new_string" after a line
+containing "search_string" in "fileName" """
     with open(filename, "r", encoding="utf-8") as f:
         line_list = []
         while True:
