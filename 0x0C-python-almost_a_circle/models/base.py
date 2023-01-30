9f92cc911d9eafa386b96d6bf72a600ea6425d41
paulsunday <paulsundayigwegbe@gmail.com>
2023-01-29 23:56:06 -0800

    FIX

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """Contains a class called Base"""
-import json
-import csv
-import os.path
 
 
 class Base:
     """Class base for other classes"""
-    self.__nb_objects = 0
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """This is an init method"""
-        if id != None:
+        if id is not iNone:
             self.id = id
         else:
             Base.__nb_objects += 1
