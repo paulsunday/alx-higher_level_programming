2c43709f73e5d2cd2f658760259d165cc74c6656
Igwegbe Sunday Paul <78316709+paulsunday@users.noreply.github.com>
2023-02-01 19:57:32 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python3
+"""Module for base class"""
+import json
+import csv
+import os.path
 
+
+class Base:
+    """Base class for all other classes"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
