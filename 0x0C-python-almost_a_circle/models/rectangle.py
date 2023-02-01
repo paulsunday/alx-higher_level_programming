1524899197f83866fe70c28e1a0d2eec7b9f1a90
Igwegbe Sunday Paul <78316709+paulsunday@users.noreply.github.com>
2023-02-01 20:03:25 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python3
+"""Module for Rectangle class"""
 
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Class Rectangle used to define a rectangle
+    ...
+    Attributes
+    ----------
+    width : int
+        the width of the rectangle
+    height : int
+        height of the rectangle
+    x : int
+        x coordinate
+    y : int
+        y coordinate
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor to initialize instances"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
