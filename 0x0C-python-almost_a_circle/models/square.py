d44bfa0481f5e7d195c49b94f3123f8a52a7b8f5
Igwegbe Sunday Paul <78316709+paulsunday@users.noreply.github.com>
2023-02-05 21:00:19 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,6 +27,7 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+        
     def update(self, *args, **kwargs):
         """Update the Square.
         Args:
